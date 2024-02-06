2e62db2d329ee27d3e3dae46fd0ddc8af8882f76
Petter Daae <petter.daae@bekk.no>
2024-02-06 09:00:25 +0100

    Remove tags in favor of default tags

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,21 +2,10 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-locals {
-  application_name = "simple"
-}
-
 module "dynamodb" {
   source = "../../"
 
-  application_name = local.application_name
-  table_name       = "table"
-
-  hash_key = "PrimaryKey"
-
+  table_name   = "table"
+  hash_key     = "PrimaryKey"
   billing_mode = "PAY_PER_REQUEST"
-
-  tags = {
-    Example = "Simple"
-  }
 }
